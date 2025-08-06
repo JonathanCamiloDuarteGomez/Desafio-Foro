a4e047434b3446e6a132c975437d6ad127e3c3cd
JonathanCamiloDuarteGomez <gjonathand@udistrital.edu.co>
2025-08-06 15:53:25 -0500

    Se acreo el build del proyecto con maven, me genero un .jar que se puede ejecutar en cualquier pc teniendo en cuenta que el pc debe tener jdk 24 y mysql, ademas para correlo se debe tener el comando guardado en el comentario de run aplicacion

--- src/main/java/com/cdg/Foro/ForoApplication.java
+++ src/main/java/com/cdg/Foro/ForoApplication.java
@@ -9,5 +9,5 @@ public class ForoApplication {
 	public static void main(String[] args) {
 		SpringApplication.run(ForoApplication.class, args);
 	}
-
+	//java -jar .\target\Foro-0.0.1-SNAPSHOT.jar --spring.profiles.active=prod --DATASOURCE_URL=jdbc:mysql://localhost/foro --DATASOURCE_USERNAME=root --DATASOURCE_PASSWORD=root
 }
